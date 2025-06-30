aef034074fb9e2998c5cd12ea8bb699bc6bb67cb
unksnow <ba.martinezm@duocuc.cl>
2025-06-29 22:36:53 -0400

    v10

--- src/main/java/com/cursos/assembler/CursoModelAssembler.java
+++ src/main/java/com/cursos/assembler/CursoModelAssembler.java
@@ -1,11 +1,8 @@
 package com.cursos.assembler;
 
 import org.springframework.hateoas.server.RepresentationModelAssembler;
-<<<<<<< HEAD
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-=======
->>>>>>> 35157af14d2e7e82086a12acdc57846cfc714145
 import org.springframework.stereotype.Component;
 
 import com.cursos.controller.CursoController;
@@ -13,12 +10,6 @@ import com.cursos.dto.Curso;
 import com.cursos.dto.CursoModel;
 
 
-<<<<<<< HEAD
-=======
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
-
-
->>>>>>> 35157af14d2e7e82086a12acdc57846cfc714145
 
 @Component
 public class CursoModelAssembler implements RepresentationModelAssembler<Curso, CursoModel> {
@@ -29,17 +20,10 @@ public class CursoModelAssembler implements RepresentationModelAssembler<Curso, 
         model.setIdcurso(curso.getIdcurso());
         model.setNombreCurso(curso.getNombreCurso());
         model.setProfesor(curso.getProfesor());
-<<<<<<< HEAD
         model.setAsignaturas(curso.getAsignaturas());
 
         model.add(linkTo(methodOn(CursoController.class).buscarCursos(curso.getIdcurso())).withSelfRel());
         model.add(linkTo(methodOn(CursoController.class).obtenerCursos()).withRel("Cursos"));
-=======
-        model.setAsignatura(curso.getAsignatura());
-
-        model.add(linkTo(methodOn(CursoController.class).buscarCurso(curso.getIdcurso())).withSelfRel());
-        model.add(linkTo(methodOn(CursoController.class).obteneCursos()).withRel("Cursos"));
->>>>>>> 35157af14d2e7e82086a12acdc57846cfc714145
         model.add(linkTo(methodOn(CursoController.class).eliminarCurso(curso.getIdcurso())).withRel("Eliminar"));
         model.add(linkTo(methodOn(CursoController.class).actualizarCurso(curso.getIdcurso(), curso)).withRel("Actualizar"));
 
